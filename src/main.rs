9c3cc9a4e4d3b5c1319a4c30ac2764e114655b84
Takanori Nakanowatari <nakanowatari@gmail.com>
2021-04-05 21:46:08 +0900

    9-2-2 Lex_impl

--- src/main.rs
+++ src/main.rs
@@ -88,6 +88,121 @@ impl LexError {
     }
 }
 
+/// 字句解析器
+fn lex(input: &str) -> Result<Vec<Token>, LexError> {
+    // 解析結果を保存するベクタ
+    let mut tokens = Vec::new();
+    // 入力
+    let input = input.as_bytes();
+    // 位置を管理する値
+    let mut pos = 0;
+    // サブレキサを呼んだ後posを更新するマクロ
+    macro_rules! lex_a_token {
+        ($lexer:expr) => {{
+            let (tok, p) = $lexer?;
+            tokens.push(tok);
+            pos = p;
+        }};
+    }
+    while pos < input.len() {
+        // ここでそれぞれの関数にinputとposを渡す
+        match input[pos] {
+            // 遷移図通りの実装
+            b'0'...b'9' => lex_a_token!(lex_number(input, pos)),
+            b'+' => lex_a_token!(lex_plus(input, pos)),
+            b'-' => lex_a_token!(lex_minus(input, pos)),
+            b'*' => lex_a_token!(lex_asterisk(input, pos)),
+            b'/' => lex_a_token!(lex_slash(input, pos)),
+            b'(' => lex_a_token!(lex_lparen(input, pos)),
+            b')' => lex_a_token!(lex_rparen(input, pos)),
+            // 空白を扱う
+            b' ' | b'\n' | b'\t' => {
+                let ((), p) = skip_spaces(input, pos)?;
+                pos = p;
+            }
+            // それ以外が来たらエラー
+            b => return Err(LexError::invalid_char(b as char, Loc(pos, pos + 1))),
+        }
+    }
+    Ok(tokens)
+}
+
+/// posのバイトが期待するものであれば1バイト消費してposを1進める
+fn consume_byte(input: &[u8], pos: usize, b: u8) -> Result<(u8, usize), LexError> {
+    // posが入力サイズ以上なら入力が終わっている
+    // 1バイト期待しているのに終わっているのでエラー
+    if input.len() <= pos {
+        return Err(LexError::eof(Loc(pos, pos)));
+    }
+    // 入力が期待するものでなければエラー
+    if input[pos] != b {
+        return Err(LexError::invalid_char(
+            input[pos] as char,
+            Loc(pos, pos + 1),
+        ));
+    }
+    Ok((b, pos + 1))
+}
+
+fn lex_plus(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+// Result::mapを使うことで結果が正常だった場合の処理を簡潔に書ける
+// これはこのコードと等価
+// ```
+// match consume_byte(input, start, b'+') {
+//     Ok((_, end)) => (Token::plus(Loc(start, end)), end),
+    //     Err(err) => Err(err),
+// }
+    consume_byte(input, start, b'+').map(|(_, end)| (Token::plus(Loc(start, end)), end))
+}
+fn lex_minus(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b'-').map(|(_, end)| (Token::minus(Loc(start, end)), end))
+}
+fn lex_asterisk(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b'*').map(|(_, end)| (Token::asterisk(Loc(start, end)), end))
+}
+fn lex_slash(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b'/').map(|(_, end)| (Token::slash(Loc(start, end)), end))
+}
+fn lex_lparen(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b'(').map(|(_, end)| (Token::lparen(Loc(start, end)), end))
+}
+fn lex_rparen(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b')').map(|(_, end)| (Token::rparen(Loc(start, end)), end))
+}
+
+fn lex_number(input: &[u8], mut pos: usize) -> (Token, usize) {
+    use std::str::from_utf8;
+    let start = pos;
+    // 入力に数字が続く限り位置を進める
+    while pos < input.len() && b"1234567890".contains(&input[pos]) {
+        pos += 1;
+    }
+    // 数字の列を数値に変換する
+    let n = from_utf8(&input[start..pos])
+        // start..posの構成からfrom_utf8は常に成功するためunwrapしても安全
+        .unwrap()
+        .parse()
+        // 同じく構成からparseは常に成功する
+        .unwrap();
+    (Token::number(n, Loc(start, pos)), pos)
+}
+
+fn skip_spaces(input: &[u8], mut pos: usize) -> ((), usize) {
+    // 入力に空白文字が続く限り位置を進める
+    while pos < input.len() && b" \n\t".contains(&input[pos]) {
+        pos += 1;
+    }
+    // そのまま空白を無視する
+    ((), pos)
+}
+
+fn recognize_many(input: &[u8], mut pos: usize, mut f: impl FnMut(u8) -> bool) -> usize {
+    while pos < input.len() && f(input[pos]) {
+        pos += 1;
+    }
+    pos
+}
+
 fn main() {
     println!("Hello, world!");
 }
