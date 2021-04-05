17856ae64edc7e4c187d65c2efa05d82b4ee7f47
Takanori Nakanowatari <nakanowatari@gmail.com>
2021-04-05 21:59:00 +0900

    9-2-2 Debug output

--- src/main.rs
+++ src/main.rs
@@ -219,6 +219,31 @@ fn test_lexer() {
     )
 }
 
+use std::io;
+/// プロンプトを表示しユーザの入力を促す
+fn prompt(s: &str) -> io::Result<()> {
+    use std::io::{stdout, Write};
+    let stdout = stdout();
+    let mut stdout = stdout.lock();
+    stdout.write(s.as_bytes())?;
+    stdout.flush()
+}
+
 fn main() {
-    println!("Hello, world!");
+    use std::io::{stdin, BufRead, BufReader};
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let stdin = BufReader::new(stdin);
+    let mut lines = stdin.lines();
+    loop {
+        prompt("> ").unwrap();
+        // ユーザの入力を取得する
+        if let Some(Ok(line)) = lines.next() {
+            // 字句解析を行う
+            let token = lex(&line);
+            println!("{:?}", token);
+        } else {
+            break;
+        }
+    }
 }
